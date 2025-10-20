0c8a441f3819e935dfe41e2627c34f22a9d5ada8
IlyasGridHQ <ilyasgrid.highness@gmail.com>
2025-10-20 09:48:10 +0100

    setting the keynote-service and creating the entity

---
+++ Keynote-service/src/main/java/com/ilyas/Keynote_service/KeynoteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ilyas.Keynote_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeynoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeynoteServiceApplication.class, args);
+	}
+
+}
---
+++ Keynote-service/src/main/java/com/ilyas/Keynote_service/entities/Keynote.java
@@ -0,0 +1,23 @@
+package com.ilyas.Keynote_service.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Entity
+@AllArgsConstructor @NoArgsConstructor @Builder
+public class Keynote {
+    @Id @GeneratedValue  (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String profession;
+}
