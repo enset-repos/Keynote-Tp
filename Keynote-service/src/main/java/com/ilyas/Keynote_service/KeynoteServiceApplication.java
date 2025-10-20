ab80cc167379b61478adcd62a09fa93931e3cc1f
IlyasGridHQ <ilyasgrid.highness@gmail.com>
2025-10-20 09:59:20 +0100

    seeding the database with commandlinerunner

--- Keynote-service/src/main/java/com/ilyas/Keynote_service/KeynoteServiceApplication.java
+++ Keynote-service/src/main/java/com/ilyas/Keynote_service/KeynoteServiceApplication.java
@@ -1,13 +1,36 @@
 package com.ilyas.Keynote_service;
 
+import com.ilyas.Keynote_service.entities.Keynote;
+import com.ilyas.Keynote_service.repository.KeynoteRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KeynoteServiceApplication {
+    KeynoteRepo keynoteRepo;
 
-	public static void main(String[] args) {
-		SpringApplication.run(KeynoteServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KeynoteServiceApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return args -> {
+            keynoteRepo.save(Keynote.builder()
+                    .firstName("ilyas")
+                    .lastName("grid")
+                    .email("ilyas@grid.com")
+                    .profession("student")
+                    .build());
+            keynoteRepo.save(Keynote.builder()
+                    .firstName("Ismail")
+                    .lastName("Zabiri")
+                    .email("test@example.com")
+                    .profession("Footballer")
+                    .build());
+            keynoteRepo.save(Keynote.builder().firstName("Mohammed").lastName("Youssfi").email("youssefi@enset.ma").profession("professor").build());
+        };
+    }
 }
